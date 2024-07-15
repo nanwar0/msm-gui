c9dbbcbac0e0a8d5a95afecd1af205b44cd2dfe1
nanwar0 <nina.anwar@chicagobooth.edu>
2024-07-15 20:05:54 +0000

    add a movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,23 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+  
+  def add
+    x = Movie.new
+
+    x.title = params.fetch("title")
+    x.year = params.fetch("year")
+    x.duration = params.fetch("duration")
+    x.description = params.fetch("description")
+    x.image = params.fetch("image")
+    x.director_id = params.fetch("director_id")
+
+    x.save
+
+    matching_movies = Movie.all
+    @list_of_movies = matching_movies.order({ :created_at => :desc })
+
+    render({ :template => "movie_templates/index" })
+  end
+
 end
